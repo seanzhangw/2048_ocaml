3ae6c3236470e7e1148859b4883c0c0dd2968908
Sean Zhang <swz9@cornell.edu>
2023-10-18 20:56:18 -0400

    grid

--- lib/game.ml
+++ lib/game.ml
@@ -1,19 +1,56 @@
 open Raylib
 open Color
+open Constants
+
 
 let setup () =
-  Raylib.init_window 800 400 "raylib [core] example - basic window";
-  Raylib.set_target_fps 60
+  init_window screen_width screen_height "raylib [core] example - basic window"; 
+  set_target_fps 60
+
+let draw_grid () =
+  let extended_square_size = square_size + spacing in (* includes the size of the square plus spacing *)
+
+  let grid_size = num_squares * extended_square_size in
+
+  (* Calculate positions for centering the grid on the screen *)
+  let grid_x = (screen_width - grid_size + spacing) / 2 in
+  let grid_y = (screen_height - grid_size + spacing) / 2 + 20 in
+
+  (* Draw dark gray squares including the margin area *)
+  for i = 0 to num_squares - 1 do
+    for j = 0 to num_squares - 1 do
+      let x = grid_x + (i * extended_square_size) in
+      let y = grid_y + (j * extended_square_size) in
+
+      (* Draw the larger background square in dark gray *)
+      Raylib.draw_rectangle (x-spacing) (y-spacing) (extended_square_size + spacing) (extended_square_size + spacing) (Color.brown); (* dark gray *)
+    done;
+  done;
+
+  (* Draw the regular squares on top of the dark gray background squares *)
+  for i = 0 to num_squares - 1 do
+    for j = 0 to num_squares - 1 do
+      let x = grid_x + (i * extended_square_size) in
+      let y = grid_y + (j * extended_square_size) in
+
+      (* Draw the smaller square with black lines *)
+      Raylib.draw_rectangle x y square_size square_size Raylib.Color.beige;
+    done;
+  done;
+  ()
 
 let rec loop () =
-  if Raylib.window_should_close () then Raylib.close_window ()
+  if window_should_close () then close_window ()
   else
-    let open Raylib in
-    begin_drawing ();
-    clear_background Color.raywhite;
-    draw_text "Congrats! You created your first window!" 190 200 20
-      Color.lightgray;
-    end_drawing ();
-    loop ()
+    begin 
+      begin_drawing ();
+      clear_background Color.white;
+      draw_grid (); 
+      end_drawing ();
+      loop () 
+    end 
 
-let () = setup () |> loop
+(* Entry point *)
+let () = 
+  setup (); 
+  loop ();
