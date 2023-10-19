9d80ac66ba60e08eba76a058aeb833bc7fa082d2
Dora <cyweng03@gmail.com>
2023-10-19 00:15:13 -0400

    fixed merge issue

--- lib/board.ml
+++ lib/board.ml
@@ -91,93 +91,66 @@ let display_tiles (tiles : int list list) =
     done
   done
 
-  let display_tiles ()= 
-    let tiles = [[0; 2; 4; 8]; [16; 32; 64; 128]; [256; 512; 1024; 2048]; [0; 0; 0; 0]] in
-    let extended_square_size = square_size + spacing in (* includes the size of the square plus spacing *)
-  
-    let grid_size = num_squares * extended_square_size in
-  
-    (* Calculate positions for centering the grid on the screen *)
-    let grid_x = (screen_width - grid_size + spacing) / 2 in
-    let grid_y = (screen_height - grid_size + spacing) / 2 + 20 in
-      let rec nth list index = 
-        match list with 
-        | [] -> failwith "Out of bounds"
-        | head :: tail -> if index = 0 then head
-        else nth tail (index - 1)
-      in
-      for i = 0 to 3 do
-        let col = nth tiles i in
-          for j = 0 to 3 do
-            let x = grid_x + (j * extended_square_size) in
-            let y = grid_y + (i * extended_square_size) in
-            let value = nth col j in
-            let color_of_value value =
-              match value with
-              | 2 -> Raylib.Color.skyblue
-              | 4 -> Raylib.Color.blue
-              | 8 -> Raylib.Color.darkblue
-              | 16 -> Raylib.Color.darkpurple
-              | 32 -> Raylib.Color.purple
-              | 64 -> Raylib.Color.violet
-              | 128 -> Raylib.Color.maroon
-              | 256 -> Raylib.Color.magenta
-              | 512 -> Raylib.Color.pink
-              | 1024 -> Raylib.Color.orange
-              | 2048 -> Raylib.Color.red
-              | _ -> Raylib.Color.beige
-            in 
-            Raylib.draw_rectangle x y square_size square_size (color_of_value value);
+let check_new_game_button_click () =
+  (* If the mouse is over the button and the left mouse button is pressed *)
+  if Raylib.is_mouse_button_pressed MouseButton.Left then
+    let mouse_x = Raylib.get_mouse_x () in
+    let mouse_y = Raylib.get_mouse_y () in
+    if
+      mouse_x >= 600
+      && mouse_x <= 600 + 150
+      && mouse_y >= 100
+      && mouse_y <= 100 + 50
+    then
+      (* Reset the board to all zeroes *)
+      board :=
+        [ [ 0; 0; 0; 0 ]; [ 0; 0; 0; 0 ]; [ 0; 0; 0; 0 ]; [ 0; 0; 0; 0 ] ]
 
-            let show = string_of_int value in
-            if value <> 0 then
-          Raylib.draw_text show (x + (square_size / 2) - ((Raylib.measure_text show 30) / 2)) 
-          (y + (square_size / 2) - 15) 30 Raylib.Color.white;
-          done
-      done 
-        let display_tiles_input (tiles : int list list) = 
-          let extended_square_size = square_size + spacing in (* includes the size of the square plus spacing *)
-        
-          let grid_size = num_squares * extended_square_size in
-        
-          (* Calculate positions for centering the grid on the screen *)
-          let grid_x = (screen_width - grid_size + spacing) / 2 in
-          let grid_y = (screen_height - grid_size + spacing) / 2 + 20 in
-            let rec nth list index = 
-              match list with 
-              | [] -> failwith "Out of bounds"
-              | head :: tail -> if index = 0 then head
-              else nth tail (index - 1)
-            in
-            for i = 0 to 3 do
-              let col = nth tiles i in
-                for j = 0 to 3 do
-                  let x = grid_x + (j * extended_square_size) in
-                  let y = grid_y + (i * extended_square_size) in
-                  let value = nth col j in
-                  let color_of_value value =
-                    match value with
-                    | 2 -> Raylib.Color.skyblue
-                    | 4 -> Raylib.Color.blue
-                    | 8 -> Raylib.Color.darkblue
-                    | 16 -> Raylib.Color.darkpurple
-                    | 32 -> Raylib.Color.purple
-                    | 64 -> Raylib.Color.violet
-                    | 128 -> Raylib.Color.maroon
-                    | 256 -> Raylib.Color.magenta
-                    | 512 -> Raylib.Color.pink
-                    | 1024 -> Raylib.Color.orange
-                    | 2048 -> Raylib.Color.red
-                    | _ -> Raylib.Color.beige
-                  in 
-                  Raylib.draw_rectangle x y square_size square_size (color_of_value value);
-      
-                  let show = string_of_int value in
-                  if value <> 0 then
-                Raylib.draw_text show (x + (square_size / 2) - ((Raylib.measure_text show 30) / 2)) 
-                (y + (square_size / 2) - 15) 30 Raylib.Color.white;
-                done
-            done
+let display_tiles_input (tiles : int list list) =
+  let extended_square_size = square_size + spacing in
+
+  (* includes the size of the square plus spacing *)
+  let grid_size = num_squares * extended_square_size in
+
+  (* Calculate positions for centering the grid on the screen *)
+  let grid_x = (screen_width - grid_size + spacing) / 2 in
+  let grid_y = ((screen_height - grid_size + spacing) / 2) + 20 in
+  let rec nth list index =
+    match list with
+    | [] -> failwith "Out of bounds"
+    | head :: tail -> if index = 0 then head else nth tail (index - 1)
+  in
+  for i = 0 to 3 do
+    let col = nth tiles i in
+    for j = 0 to 3 do
+      let x = grid_x + (j * extended_square_size) in
+      let y = grid_y + (i * extended_square_size) in
+      let value = nth col j in
+      let color_of_value value =
+        match value with
+        | 2 -> Raylib.Color.skyblue
+        | 4 -> Raylib.Color.blue
+        | 8 -> Raylib.Color.darkblue
+        | 16 -> Raylib.Color.darkpurple
+        | 32 -> Raylib.Color.purple
+        | 64 -> Raylib.Color.violet
+        | 128 -> Raylib.Color.maroon
+        | 256 -> Raylib.Color.magenta
+        | 512 -> Raylib.Color.pink
+        | 1024 -> Raylib.Color.orange
+        | 2048 -> Raylib.Color.red
+        | _ -> Raylib.Color.beige
+      in
+      Raylib.draw_rectangle x y square_size square_size (color_of_value value);
+
+      let show = string_of_int value in
+      if value <> 0 then
+        Raylib.draw_text show
+          (x + (square_size / 2) - (Raylib.measure_text show 30 / 2))
+          (y + (square_size / 2) - 15)
+          30 Raylib.Color.white
+    done
+  done
 
 let game_page () =
   draw_text "2048" 100 30 80 Color.brown;
@@ -189,6 +162,6 @@ let game_page () =
   draw_new_game_button ();
   draw_init_grid ();
   display_tiles_input !board
-  (* display_tiles !board *)
+(* display_tiles !board *)
 
 (* display_tiles board; *)
