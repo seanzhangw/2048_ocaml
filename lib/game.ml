a417accbb76ddaa75d43c83f1482e1a98c910b4d
Vicky Wang <vw92@cornell.edu>
2023-10-18 21:04:42 -0400

    change welcome window

--- lib/game.ml
+++ lib/game.ml
@@ -2,12 +2,12 @@ open Raylib
 open Color
 
 let setup () =
-    Raylib.init_window 800 400 "raylib [core] example - basic window";
+    Raylib.init_window 800 600 "raylib [core] example - basic window";
     Raylib.set_target_fps 60
 
 let starting_page () =
-    Raylib.draw_text "Welcome to My Game!" 190 100 20 Color.red;
-    Raylib.draw_text "Press Space to Start!" 190 130 20 Color.blue
+    Raylib.draw_text "Welcome to 2048!" 190 220 50 Color.brown;
+    Raylib.draw_text "Press Space to Start" 240 300 30 Color.beige
 
     let rec loop game_started =
       if Raylib.window_should_close () then 
@@ -18,7 +18,7 @@ let starting_page () =
           clear_background Color.raywhite;
           let new_game_state = 
               if game_started then (
-                  draw_text "Game Started! Do game logic here." 190 200 20 Color.green;
+                  draw_text "Game Started! Do game logic here." 190 200 20 Color.beige;
                   game_started
               ) else if is_key_pressed Key.Space then
                   true  (* update game_started to true *)
