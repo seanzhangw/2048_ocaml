6426016c1f7f4ee6b2e3630abf916471fed606d8
Vicky Wang <vw92@cornell.edu>
2023-10-18 22:33:05 -0400

    adjust spacing

--- lib/instructions.ml
+++ lib/instructions.ml
@@ -1,6 +1,7 @@
 open Raylib
 open Color
 let rec instructions () =
-    draw_text "These are the instructions..." 200 200 30 Color.black;
+    draw_text "Instructions" 200 200 30 Color.brown;
+    draw_text "These are the instructions...." 200 240 20 Color.beige;
 
 (* Return to the game loop if the Escape key is pressed *)
--- lib/start.ml
+++ lib/start.ml
@@ -2,5 +2,5 @@ open Raylib
 let starting_page () =
   Raylib.draw_text "Welcome to 2048!" 190 220 50 Color.brown;
   Raylib.draw_text "Press space to start" 240 300 30 Color.beige;
-  Raylib.draw_text "Press i for instructions" 220 380 30 Color.beige
+  Raylib.draw_text "Press i for instructions" 220 350 30 Color.beige
 
