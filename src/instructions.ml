59a3443d14669b279976f570fd8598b756347f0c
Sean Zhang <swz9@cornell.edu>
2023-12-07 19:35:55 -0500

    remove old tetris code

--- src/instructions.ml
+++ src/instructions.ml
@@ -13,17 +13,4 @@ let rec instructions () =
   draw_text "Tiles with the same number merge into" (screen_width - 680)
     (screen_height - 370) 20 Color.beige;
   draw_text "one when they touch. Add them up to reach 2048!"
-    (screen_width - 678) (screen_height - 340) 20 Color.beige;
-  (*Game instrcutions 2*)
-  draw_text "Game Mode 2: Tetris Ver. (t to start)" (screen_width - 680)
-    (screen_height - 290) 20 Color.brown;
-  draw_text "The gameplay is same like the original Tetris game."
-    (screen_width - 680) (screen_height - 240) 20 Color.beige;
-  draw_text "The blocks containing number falls from the top."
-    (screen_width - 680) (screen_height - 210) 20 Color.beige;
-  draw_text "The player needs to collapse the falling blocks in"
-    (screen_width - 680) (screen_height - 180) 20 Color.beige;
-  draw_text "left, right or downward direction. The aim is" (screen_width - 675)
-    (screen_height - 150) 20 Color.beige;
-  draw_text "to get to the 2048 tile!" (screen_width - 675)
-    (screen_height - 120) 20 Color.beige
+    (screen_width - 678) (screen_height - 340) 20 Color.beige
