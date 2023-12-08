06e04ab4d93d6be307eb7f183f44344077f3b7a8
Sean Zhang <swz9@cornell.edu>
2023-12-07 22:51:56 -0500

    correct typo

--- src/l_state.mli
+++ src/l_state.mli
@@ -1,4 +1,4 @@
 (** File housing the lose page screen and instructions for the player. *)
 
 val lose_state : unit -> unit
-(** [lose_state]] draws the lose state screen and displays instructions. *)
+(** [lose_state] draws the lose state screen and displays instructions. *)
