f7766e7a4e94aeb9905dbd43cefe1c90e9c9eb34
Sean Zhang <swz9@cornell.edu>
2023-12-07 19:34:18 -0500

    add instructions.mli; start.mli, utils.mli

---
+++ src/instructions.mli
@@ -0,0 +1,5 @@
+(* File housing the instructions for the player. *)
+
+val instructions : unit -> unit
+(** [instructions] draws the instruction text into the correct location within
+    the RayLib window. *)
