3fb27a3b45942f08a9dd7842b91e897be129416b
Vicky Wang <vw92@cornell.edu>
2023-12-09 04:28:03 -0500

    Updated instructions after deleting tetris

--- src/instructions.ml
+++ src/instructions.ml
@@ -3,14 +3,9 @@ open Constants
 open Color
 
 let rec instructions () =
-  draw_text "Instructions:" (screen_width - 680) (screen_height - 520) 40
-    Color.darkbrown;
-  (*Game instructions 1*)
-  draw_text "Game Mode 1: Original (o to start)" (screen_width - 680)
-    (screen_height - 450) 20 Color.brown;
-  draw_text "Use your arrow keys to move the tiles." (screen_width - 680)
-    (screen_height - 400) 20 Color.beige;
-  draw_text "Tiles with the same number merge into" (screen_width - 680)
-    (screen_height - 370) 20 Color.beige;
-  draw_text "one when they touch. Add them up to reach 2048!"
-    (screen_width - 678) (screen_height - 340) 20 Color.beige
+  draw_text "Instructions:" 170 200 40 Color.brown;
+  draw_text "Use your arrow keys to move the tiles." 170 270 20 Color.beige;
+  draw_text "Tiles with the same number merge into" 170 300 20 Color.beige;
+  draw_text "one when they touch. Add them up to reach 2048!" 170 330 20
+    Color.beige;
+  draw_text "Press s to start game! " 170 400 20 Color.beige
