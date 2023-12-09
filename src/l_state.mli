5f85dff104122caa5bf5619f1d33739b2e773ab7
Vicky Wang <vw92@cornell.edu>
2023-12-09 07:10:40 -0500

    Added new lose state mli

---
+++ src/l_state.mli
@@ -0,0 +1,4 @@
+(** File housing the lose page screen and instructions for the player. *)
+
+val lose_state : unit -> unit
+(** [lose_state]] draws the lose state screen and displays instructions. *)
